b2e2940ff487078e2c2bf2155385f9d5da9f9c3f
Mohd Asad Ansari <asad.as9458@gmail.com>
2024-04-06 18:00:46 +0530

    Updated

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,46 +2,36 @@ import React, { useState, useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Cookies from "js-cookie";
-// import { useUserData } from './hooks/useUserData';
 import { ThemeProvider } from "./contexts/theme";
 import Footer from "./components/Footer/Footer";
 
 function App() {
   const userDataCookie = Cookies.get("userData");
   const userData = JSON.parse(userDataCookie || "{}");
-  // console.log(userData)
 
-  const [themeMode, setThemeMode] = useState(true);
-  const [a, setA] = useState("black");
-  const [bg, setBg] = useState("bg-gray-100");
-
-  const theme = () => {
-
-  // actual change in theme
-  setThemeMode(!themeMode);
-    if (themeMode === true) {
-        setBg("bg-gray-900");
-        setA("white")
-    }
-    else {
-      setBg("bg-gray-100");
-      setA("black")
-    }
+  // Load theme mode preference from local storage or default to false if not available
+  const initialThemeMode = localStorage.getItem("toggled") === "true";
+  const [themeMode, setThemeMode] = useState(initialThemeMode);
 
   useEffect(() => {
-    // document.querySelector("html").classList.remove("light", "dark");
-    // document.querySelector("html").classList.add(themeMode  ? "light" : "dark");
+    // Set theme based on the initial theme mode preference
+    const bg = themeMode ? "bg-gray-900" : "bg-gray-100";
+    const textColor = themeMode ? "white" : "black";
+    document.body.className = `text-${textColor} ${bg}`;
+  }, [themeMode]);
 
-  }, [themeMode, bg]); 
-
-}
+  const toggleTheme = () => {
+    const toggledMode = !themeMode;
+    setThemeMode(toggledMode);
+    localStorage.setItem("toggled", toggledMode);
+  };
 
   return (
-    <ThemeProvider value={{ themeMode, theme }}>
-      <div className={`text-${a} ${bg}` } >
+    <ThemeProvider value={{ themeMode, theme: toggleTheme }}>
+      <div>
         <Header userData={userData} />
         <Outlet />
-        <Footer/>
+        <Footer />
       </div>
     </ThemeProvider>
   );
